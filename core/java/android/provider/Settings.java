bc0962e124dc3afd4b209289b00fe27bc6956a8b
ezio84-xda <brabus84@gmail.com>
2014-12-26 10:09:16 +0100

    Recents: option to disable searchbar [2/2]
    code by ProjectDISCO rom  @yashade2001
    
    Change-Id: I04e4906c3f310802952411e4d7295b2d74ea8859
    ezio: move the entry under Interface instead of Display

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3250,6 +3250,12 @@ public final class Settings {
         public static final String VOLUME_PANEL_EXPANDED = "volume_link_expanded";
 
         /**
+         * Recents Show/Hide Search Bar
+         * @hide
+         */
+        public static final String RECENTS_SHOW_HIDE_SEARCH_BAR = "recents_show_hide_search_bar";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

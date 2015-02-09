29b0a91d18220c4d1bbb36b54e316d0a34ea5d1e
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-09 13:39:52 +0000

    QS: add Battery saver tile
    Drawables by Alexander Westphal <westphal.alex94@gmail.com>
    
    Change-Id: I7bb419ac5d135b27f9f35d4a7acdb452a197a68f
    
    Conflicts:
    	core/java/com/android/internal/util/cm/QSConstants.java
    	packages/SystemUI/res/values/custom_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    
    Conflicts:
    	core/java/com/android/internal/util/slim/QSConstants.java
    	packages/SystemUI/res/values/chroma_strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
    
    Update battery saver tile
    Credit goes to Altaf-Mahdi (Euphoria), modified to not contain qsCollapsePanelhttps://github.com/Euphoria-OS/android_frameworks_base/commit/8581f2a90ea7daa7750371f69dff8f8a2013f0ba

--- core/java/com/android/internal/util/slim/QSConstants.java
+++ core/java/com/android/internal/util/slim/QSConstants.java
@@ -51,6 +51,7 @@ public class QSConstants {
     public static final String TILE_EXPANDED_DESKTOP = "expanded_desktop";
     public static final String TILE_LED = "led";
     public static final String TILE_LIVE_DISPLAY = "live_display";
+    public static final String TILE_BATTERY_SAVER = "battery_saver";
 
     // Order matters
     protected static final ArrayList<String> TILES_DEFAULT = new ArrayList<String>();
@@ -92,5 +93,6 @@ public class QSConstants {
         TILES_AVAILABLE.add(TILE_EXPANDED_DESKTOP);
         TILES_AVAILABLE.add(TILE_LED);
         TILES_AVAILABLE.add(TILE_LIVE_DISPLAY);
+        TILES_AVAILABLE.add(TILE_BATTERY_SAVER);
     }
 }
--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -39,6 +39,7 @@ import com.android.systemui.qs.tiles.AdbOverNetworkTile;
 import com.android.systemui.qs.tiles.AirplaneModeTile;
 import com.android.systemui.qs.tiles.AmbientDisplayTile;
 import com.android.systemui.qs.tiles.ApnTile;
+import com.android.systemui.qs.tiles.BatterySaverTile;
 import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
@@ -340,6 +341,8 @@ public class QSTileHost implements QSTile.Host {
                 return new LedTile(this);
             case QSConstants.TILE_LIVE_DISPLAY:
                 return new LiveDisplayTile(this);
+            case QSConstants.TILE_BATTERY_SAVER:
+                return new BatterySaverTile(this);
             default:
                 throw new IllegalArgumentException("Bad tile spec: " + tileSpec);
         }

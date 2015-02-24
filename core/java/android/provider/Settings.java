824a0e6e45570cd927972b5233783363863ab6d4
XXMrHyde <xxmrhyde@gmx.co.uk>
2015-02-24 21:33:22 +0100

    FWB: Hide network activity arrows (1/2)
    AICPfy
    Destilled from here:
    https://github.com/CyanideL/android_frameworks_base/commit/7c802f64c306a1c023a67737560b58572e46c644
    so giving all credits to the mentioned @XXMrHyde
    
    Fix for msim
    
    Change-Id: Ibb18a9b1988e5275da1757c5ba6b874be5943273
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3462,6 +3462,12 @@ public final class Settings {
         public static final String RECENTS_SHOW_HIDE_SEARCH_BAR = "recents_show_hide_search_bar";
 
         /**
+         * Show when WiFi or data mobile is sending/receiving data
+         * @hide
+         */
+        public static final String STATUS_BAR_SHOW_NETWORK_ACTIVITY = "status_bar_show_network_activity";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *

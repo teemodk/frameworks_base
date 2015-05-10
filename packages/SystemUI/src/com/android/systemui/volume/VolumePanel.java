c8e8f6bbd54612361d5476d310e2fcabd781718a
ezio84 <brabus84@gmail.com>
2015-05-10 19:16:56 +0200

    Silent mode: rename volume_adjust_sound to volume_adjust_sound_enabled
    to fit the existing volume adjust sound commit i have
    
    Change-Id: If998b1ba0ca3688f896aa14efce0bf25566255f6

--- packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
+++ packages/SystemUI/src/com/android/systemui/volume/VolumePanel.java
@@ -295,7 +295,7 @@ public class VolumePanel extends Handler implements DemoMode {
         @Override
         public void onChange(boolean selfChange) {
             mVolumeAdjustSound = Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.VOLUME_ADJUST_SOUND, 1) == 1;
+                    Settings.System.VOLUME_ADJUST_SOUND_ENABLED, 1) == 1;
             mLinkNotificationWithVolume = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.VOLUME_LINK_NOTIFICATION, 1) == 1;
             createSliders();
@@ -470,14 +470,14 @@ public class VolumePanel extends Handler implements DemoMode {
         mVoiceCapable = context.getResources().getBoolean(R.bool.config_voice_capable);
 
         mVolumeAdjustSound = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.VOLUME_ADJUST_SOUND, 1) == 1;
+                Settings.System.VOLUME_ADJUST_SOUND_ENABLED, 1) == 1;
         mLinkNotificationWithVolume = Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.VOLUME_LINK_NOTIFICATION, 1) == 1;
         mExtendedPanelExpanded = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.VOLUME_PANEL_EXPANDED, 0) == 1;
 
         context.getContentResolver().registerContentObserver(
-                Settings.System.getUriFor(Settings.System.VOLUME_ADJUST_SOUND), false,
+                Settings.System.getUriFor(Settings.System.VOLUME_ADJUST_SOUND_ENABLED), false,
                 mSettingsObserver);
         context.getContentResolver().registerContentObserver(
                 Settings.System.getUriFor(Settings.System.VOLUME_LINK_NOTIFICATION), false,

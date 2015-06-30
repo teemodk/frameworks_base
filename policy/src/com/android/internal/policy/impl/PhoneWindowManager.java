703781261ab69a7f88b8ca1047fd80c0997584d6
ezio84 <brabus84@gmail.com>
2015-06-30 11:30:58 +0200

    Revert "Frameworks: only send volume key events to the session manager if it is not a wake key"
    This reverts commit fd07932f09f13bcaa9e9d626af1f240cdb1e7d51.

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -5762,7 +5762,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                                 break;
                             }
                         }
-                        if (!isWakeKey) {
+                        if (!isScreenOn() && !mVolumeWakeScreen) {
                             // If we aren't passing to the user and no one else
                             // handled it send it to the session manager to figure
                             // out.

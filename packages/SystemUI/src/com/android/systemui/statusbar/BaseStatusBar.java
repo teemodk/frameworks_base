36ef755fd209047587f99697cbcf5e71cb049f6e
ezio84 <brabus84@gmail.com>
2015-07-02 22:12:49 +0200

    Revert "[2/2] SlimHeadsUp: Add a global switch"
    This reverts commit 1fceefa8e69fe69f3fcfdb92795b9bce398188a8.

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -207,7 +207,6 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected boolean mDisableNotificationAlerts = false;
 
     private int mHeadsUpSnoozeTime;
-    private boolean mHeadsUpGlobalSwitch;
     private long mHeadsUpSnoozeStartTime;
     protected String mHeadsUpPackageName;
 
@@ -996,12 +995,10 @@ public abstract class BaseStatusBar extends SystemUI implements
             if (isThisASystemPackage(pkg, pmUser)) {
                 headsUpButton.setVisibility(View.GONE);
             } else {
-                boolean isHeadsUpEnabledForPackage =
-                        mNoMan.getHeadsUpNotificationsEnabledForPackage(
-                            pkg, appUidF) != Notification.HEADS_UP_NEVER;
-                headsUpButton.setAlpha(isHeadsUpEnabledForPackage ? 1f : 0.5f);
-                setHeadsUpButtonContentDescription((View) headsUpButton,
-                        isHeadsUpEnabledForPackage);
+                boolean isHeadsUpEnabled = mNoMan.getHeadsUpNotificationsEnabledForPackage(
+                        pkg, appUidF) != Notification.HEADS_UP_NEVER;
+                headsUpButton.setAlpha(isHeadsUpEnabled ? 1f : 0.5f);
+                setHeadsUpButtonContentDescription((View) headsUpButton, isHeadsUpEnabled);
                 headsUpButton.setVisibility(View.VISIBLE);
                 headsUpButton.setOnClickListener(new View.OnClickListener() {
                     public void onClick(View v) {
@@ -2320,14 +2317,6 @@ public abstract class BaseStatusBar extends SystemUI implements
                 mHeadsUpSnoozeTime / 60 / 1000), Toast.LENGTH_LONG).show();
     }
 
-    protected boolean isHeadsUpEnabled() {
-        return mHeadsUpGlobalSwitch;
-    }
-
-    protected void setHeadsUpEnabled(boolean headsUpGlobalSwitch) {
-        mHeadsUpGlobalSwitch = headsUpGlobalSwitch;
-    }
-
     protected boolean isHeadsUpInSnooze() {
         return (mHeadsUpSnoozeStartTime + mHeadsUpSnoozeTime - System.currentTimeMillis()) > 0;
     }
@@ -2355,11 +2344,6 @@ public abstract class BaseStatusBar extends SystemUI implements
             if (DEBUG) {
                 Log.d(TAG, "Skipping HUN check for " + sbn.getKey() + " since it's filtered out.");
             }
-            return false;
-        }
-
-        // Stop here if headsup is globally disabled
-        if (!isHeadsUpEnabled()) {
             return false;
         }
 

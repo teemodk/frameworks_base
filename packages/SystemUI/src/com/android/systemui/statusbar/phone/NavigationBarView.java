81b1cf98e95a99f4d69cdcbda1c98e05863edd59
ezio84 <brabus84@gmail.com>
2015-07-30 19:27:29 +0200

    Revert "SlimDim: Use semaphore to cancel ontouch callback"
    This reverts commit 58cb04fc0aaca679c89765c3c72a1820a4337d18.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -113,8 +113,6 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
     private boolean mImeArrowVisibility;
     private boolean mIsImeArrowVisible = false;
 
-    private boolean mIsHandlerCallbackActive = false;
-
     final Display mDisplay;
     View mCurrentView = null;
     View[] mRotatedViews = new View[4];
@@ -239,10 +237,7 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
     };
 
     public void onNavButtonTouched() {
-        if (mIsHandlerCallbackActive) {
-            mHandler.removeCallbacks(mNavButtonDimmer);
-            mIsHandlerCallbackActive = false;
-        }
+        mHandler.removeCallbacks(mNavButtonDimmer);
         final boolean keyguardProbablyEnabled =
                 (mDisabledFlags & View.STATUS_BAR_DISABLE_HOME) != 0;
         if (getNavButtons() != null) {
@@ -255,7 +250,6 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
             }
             if (mDimNavButtons && !keyguardProbablyEnabled) {
                 mHandler.postDelayed(mNavButtonDimmer, mDimNavButtonsTimeout);
-                mIsHandlerCallbackActive = true;
             }
         }
     }
@@ -1291,7 +1285,6 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
     private Runnable mNavButtonDimmer = new Runnable() {
         @Override
         public void run() {
-            mIsHandlerCallbackActive = false;
             if (getNavButtons() != null && mIsDim == false) {
                 mIsDim = true;
                 if (mDimNavButtonsAnimate) {

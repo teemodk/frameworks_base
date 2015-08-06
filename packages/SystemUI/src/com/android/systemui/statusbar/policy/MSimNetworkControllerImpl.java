605141bd353077d32d26be575080b27da661ac6d
ezio84 <brabus84@gmail.com>
2015-08-06 20:00:32 +0200

    Revert "Change arguments order in setMobileDataIndicators"
    This reverts commit 2a4f9cb7499b195884680c72045547a46fcea0e3.

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkControllerImpl.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/MSimNetworkControllerImpl.java
@@ -356,8 +356,8 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
             cluster.setMobileDataIndicators(
                     true,
                     mAlwaysShowCdmaRssi ? mPhoneSignalIconId : mWimaxIconId,
-                    mMSimMobileActivityIconId[phoneId],
                     mInetCondition,
+                    mMSimMobileActivityIconId[phoneId],
                     mMSimDataTypeIconId[phoneId],
                     mMSimDataRoamIconId[phoneId],
                     mContentDescriptionWimax,
@@ -370,8 +370,8 @@ public class MSimNetworkControllerImpl extends NetworkControllerImpl {
                     mHasMobileDataFeature,
                     mShowPhoneRSSIForData ? mMSimPhoneSignalIconId[phoneId]
                         : mMSimDataSignalIconId[phoneId],
-                    mMSimMobileActivityIconId[phoneId],
                     mInetCondition,
+                    mMSimMobileActivityIconId[phoneId],
                     mMSimDataTypeIconId[phoneId],
                     mMSimDataRoamIconId[phoneId],
                     mMSimContentDescriptionPhoneSignal[phoneId],

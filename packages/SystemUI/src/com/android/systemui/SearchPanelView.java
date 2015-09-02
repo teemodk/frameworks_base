c22a5c6e6d17bbb1901959a1301e98680682dc6b
ezio84 <brabus84@gmail.com>
2015-09-02 15:24:31 +0200

    build fix
    Change-Id: Ib2017eeb44a68f217bc9443f625b00c4506fd0e8

--- packages/SystemUI/src/com/android/systemui/SearchPanelView.java
+++ packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -350,7 +350,7 @@ public class SearchPanelView extends FrameLayout implements StatusBarPanel,
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
                 if (mDraggedFarEnough && (mTargetActivities[mCircle.mIntersectIndex] != null &&
-                        !ACTION_NONE.equals(mTargetActivities[mCircle.mIntersectIndex]))) {
+                        !ACTION_NULL.equals(mTargetActivities[mCircle.mIntersectIndex]))) {
                     if (mCircle.isAnimationRunning(true  /* enterAnimation */)) {
                         mLaunchPending = true;
                         mCircle.setAnimatingOut(true);
